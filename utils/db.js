098ff4b96a78ead301e98d1a373012754613cc91
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2024-06-18 13:12:05 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -5,16 +5,16 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cachedClient = null;
 
 export async function connectDB() {
-  if (cachedClient) {
-    return cachedClient;
-  }
+  // if (cachedClient) {
+  //   return cachedClient;
+  // }
 
   const client = await mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
-  cachedClient = client;
+  // cachedClient = client;
 
   return client;
 }
