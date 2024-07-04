f15e45a87e3068fc38c90d5e739dd234c804083d
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2024-07-04 11:50:57 +0530

    caching fix attempt

--- app/api/all-notes/route.js
+++ app/api/all-notes/route.js
@@ -1,15 +1,19 @@
 import { NextResponse } from 'next/server';
 import { connectDB } from '../../../utils/db';
 import { getAllNotes } from '@/lib/functions/note';
+
 let isConnected = false;
 if (!isConnected) {
   connectDB();
   isConnected = true;
 }
+
 export async function GET() {
   try {
     const foundNotes = await getAllNotes();
-    return NextResponse.json(foundNotes);
+    const response = NextResponse.json(foundNotes);
+    response.headers.set('Cache-Control', 'no-store, max-age=0');
+    return response;
   } catch (err) {
     return NextResponse.json(
       { error: 'An error occurred while fetching Notes' },
